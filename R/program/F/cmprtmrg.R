8a2a38dbdd26c09a21906313a79818799e61caf9
KumikoAgata <kumiko.agata@nnh.go.jp>
2019-08-21 10:27:07 +0900

    2019.08.21　プログラムUP

--- R/program/F/cmprtmrg.R
+++ R/program/F/cmprtmrg.R
@@ -1,13 +1,13 @@
 #*******************************************
 #
-# プログラム名：cmallmrg.R  Ver1.0
+# プログラム名：cmprtmrg.R  Ver1.0
 # 概要　　　　：J-TALC2で入力した薬剤名（商品名）と一般名の対照表を作成
-#             　薬剤名と一般名の完全一致を取った場合のプログラム
+#             　薬剤名と一般名の部分一致を取った場合のプログラム
 #               input　 J-TALC2_cm_190725_1055.csv、idf_20190422.sas7bdat
-#　　　　　　　 output  cmallmrg.csv
+#　　　　　　　 output  cmprtmrg.csv
 #
 # 履歴
-#　　Ver1.0　2019.08.21　Agata.K　作成（19.08.21 コメント等分かりづらい部分更新）
+#　　Ver1.0　2019.08.21　Agata.K　作成途中
 #
 #*******************************************　
 
@@ -22,7 +22,9 @@ prtPath <- "//ARONAS/Stat/Trials/Chiken/J-TALC2"
 #  ファイル名定義
 rfname <- paste0(prtPath, "/input/rawdata/J-TALC2_cm_190725_1055.csv")
 efname <- paste0(prtPath, "/input/ext/idf_20190422.sas7bdat")
-outfname <- paste0(prtPath, "/output/F/cmallmrg.csv")
+outfname <- paste0(prtPath, "/output/F/cmprtmrg.csv")
+#  一致文字数定義
+match <- 5
 
 
 # ファイル読込(Ptosh/SAS)
@@ -30,13 +32,21 @@ rdata <- read.csv(rfname, as.is=T, na.strings="")
 edata <- read.sas7bdat(efname, debug=FALSE)
 
 
-# マージ （一致した項目のみ表示）Key＝Ptosh：薬剤名、SAS：一般名
-re.merge <- merge(rdata, edata,  by.x="薬剤名", by.y="MEDDRUGFULL", is.na="")
-# CSVファイル出力
+# 一致確認
+#  Ptoshデータ数分回す（for)
+
+#  先頭からmatch分の比較文字抽出(Ptoshデータ)
+
+#  SASデータ数分回す（for）
+
+#  先頭からmatch分の比較文字抽出(SASデータ)
+
+#  一致確認
+
+#  一致なら
+
+#  不一致なら
+
+
+# CSVファイル出力（Ptoshデータ,一致一般名①,一致一般名②,・・・）
 write.csv(re.merge, outfname, row.names=T, na="")
-
-
-# マージ (Ptoshの情報は全て表示)
-#re.merge <- merge(rdata, edata,  by.x="薬剤名", by.y="MEDDRUGFULL", all.x=T, is.na="")
-# CSVファイル出力
-#write.csv(re.merge, outfname, row.names=T, na="")
